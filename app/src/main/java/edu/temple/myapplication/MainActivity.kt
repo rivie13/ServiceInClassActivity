6dc55b8f10c0d0bc570465a7fa2bdf3aafd80157
Riviera Sperduto <riviera.t.sperduto13@gmail.com>
2025-04-03 12:01:59 -0400

    Implemented TimerService binding and UI updates. Handler was implemented to properly show data on the screen.

--- app/src/main/java/edu/temple/myapplication/MainActivity.kt
+++ app/src/main/java/edu/temple/myapplication/MainActivity.kt
@@ -5,8 +5,12 @@ import android.content.Context
 import android.content.Intent
 import android.content.ServiceConnection
 import android.os.Bundle
+import android.os.Handler
 import android.os.IBinder
+import android.os.Looper
+import android.os.Message
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -15,10 +19,18 @@ class MainActivity : AppCompatActivity() {
     private var isBound = false
     private lateinit var startButton: Button
     private lateinit var stopButton: Button
+    private lateinit var timerTextView: TextView
+
+    private val handler = object : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            timerTextView.text = msg.what.toString()
+        }
+    }
 
     private val connection = object : ServiceConnection {
         override fun onServiceConnected(className: ComponentName, service: IBinder) {
             timerBinder = service as TimerService.TimerBinder
+            timerBinder?.setHandler(handler)
             isBound = true
         }
 
@@ -34,6 +46,7 @@ class MainActivity : AppCompatActivity() {
 
         startButton = findViewById(R.id.startButton)
         stopButton = findViewById(R.id.stopButton)
+        timerTextView = findViewById(R.id.textView)
 
         val serviceIntent = Intent(this, TimerService::class.java)
         bindService(serviceIntent, connection, Context.BIND_AUTO_CREATE)
@@ -41,7 +54,7 @@ class MainActivity : AppCompatActivity() {
         startButton.setOnClickListener {
             if (isBound) {
                 if (!timerBinder!!.isRunning) {
-                    timerBinder?.start(5) // Start with 10 seconds
+                    timerBinder?.start(10) // Start with 10 seconds
                     startButton.text = "Pause"
                 } else if (timerBinder!!.paused) {
                     timerBinder?.pause()
@@ -57,6 +70,7 @@ class MainActivity : AppCompatActivity() {
             if (isBound) {
                 timerBinder?.stop()
                 startButton.text = "Start"
+                timerTextView.text = "0"
             }
         }
     }
